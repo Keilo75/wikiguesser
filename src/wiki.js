960f397d6f4dcbc7fac4d843be5de29a887a6071
Keilo75 <geschichtler10@web.de>
2021-06-05 16:20:07 +0200

    change: fix problems with formatting

--- src/wiki.js
+++ src/wiki.js
@@ -1,7 +1,9 @@
 const fetch = require('node-fetch');
 const url = 'https://en.wikipedia.org/api/rest_v1/page';
 
-async function getRandomArticles() {   
+async function getRandomArticles() {      
+  return formatResponse('Berowra Waters, New South Wales', 'Berowra Waters is an outer suburb of Northern Sydney, in the state of New South Wales, Australia. Berowra is located 40 kilometres north of the Sydney central business district, in the local government area of Hornsby Shire. Berowra Waters is north-west of the suburbs of Berowra Heights and west of Berowra.')
+  
   const articles = {
     text: '',
     list: []
@@ -44,31 +46,36 @@ function formatResponse(title, text) {
     const splittedText = text.split(' ');
 
     for (word of splittedText) {
-      let isForbidden = word.toLowerCase().replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, '').startsWith(forbiddenWord.toLowerCase().replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, ''));
+      // Check if word is longer
+      let isForbidden;
+      if (forbiddenWord.replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, '').length <= word.replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, '').length) {
+        isForbidden = forbiddenWord.toLowerCase().replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, '').startsWith(word.replace(/[&\/\\#,+()$~%.'":*!?<>{}]/g, '').toLowerCase());
+      }   
 
-      // Check initals
-      if (forbiddenWord.length === 2 && forbiddenWord[1] === '.') isForbidden = word.startsWith(forbiddenWord[0]);
+      // Edge cases
+      // Bueller's has to detect Bueller
+      // Arena should not detect 'a'
       
       if (!isForbidden) continue;
       
       const wordIndex = splittedText.indexOf(word);
-      // Check if previous element was already removed and remove current element
-      if (splittedText[wordIndex - 1] === '???') {
-        splittedText.splice(wordIndex, 1);
-        continue;
-      }
 
       // Handle commas and points
+      const specialCharacters = /[.:,;?!"']/;
       const lastCharacter = word[word.length - 1];
-      const isPunctuationMark1 = lastCharacter.match(/[.:,;?!"]/);
-
+      const isPunctuationMark1 = lastCharacter?.match(specialCharacters);
+      
       const firstCharacter = word[0];
-      const isPunctuationMark2 = firstCharacter.match(/[.:,;?!"]/);
-
-    
+      const isPunctuationMark2 = firstCharacter.match(specialCharacters);
+      
       // If word includes a forbidden word, replace it
-      splittedText[wordIndex] = (isPunctuationMark2 ? firstCharacter : '') + '???' + (isPunctuationMark1 ? lastCharacter : '');
+      splittedText[wordIndex] = (isPunctuationMark2 ? firstCharacter : '') + '___' + (isPunctuationMark1 ? lastCharacter : '');
+      
+      // Check if last word is already removed
+      if (splittedText[wordIndex - 1]?.includes('___')) splittedText[wordIndex] = (isPunctuationMark2 ? firstCharacter : '') + (isPunctuationMark1 ? lastCharacter : '');
+    
     }
+
     text = splittedText.join(' ');
     
   }
