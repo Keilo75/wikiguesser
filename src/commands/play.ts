59aaa4f63ca668515ff7a4cf9dffa71b44f5dc94
Keilo75 <82605039+Keilo75@users.noreply.github.com>
2025-01-20 20:41:36 +0100

    feat: show buttons

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,7 +1,17 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Colors,
+  EmbedBuilder,
+  type MessageActionRowComponentBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import { t } from "i18next";
 
 import { type Command } from "../models/command";
+
+const GUESS_TIME_SECONDS = 10;
 
 export const play: Command = {
   data: new SlashCommandBuilder()
@@ -9,6 +19,28 @@ export const play: Command = {
     .setDescription(t("commands.play")),
   execute: async ({ interaction, cache }) => {
     const game = await cache.getGame(interaction.guildId);
-    await interaction.reply("xd");
+
+    const embed = new EmbedBuilder()
+      .setTitle(t("play.title", { seconds: GUESS_TIME_SECONDS }))
+      .setColor(Colors.White)
+      .setDescription(game.censoredText)
+      .setFooter({
+        text: interaction.user.displayName,
+        iconURL: interaction.user.displayAvatarURL({ forceStatic: true }),
+      });
+
+    const buttons = game.options.map((opt) =>
+      new ButtonBuilder()
+        .setCustomId(opt)
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(opt)
+    );
+
+    const row =
+      new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+        buttons
+      );
+
+    await interaction.reply({ embeds: [embed], components: [row] });
   },
 };
