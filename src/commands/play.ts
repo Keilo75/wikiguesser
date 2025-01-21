dc25fb7a5f0433f199eb2f82099c85ee6215334f
Keilo75 <82605039+Keilo75@users.noreply.github.com>
2025-01-21 18:52:34 +0100

    feat: show timeout embed

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -19,15 +19,16 @@ export const play: Command = {
     .setDescription(t("commands.play")),
   execute: async ({ interaction, cache }) => {
     const game = await cache.getGame(interaction.guildId);
+    const footer = {
+      text: interaction.user.displayName,
+      iconURL: interaction.user.displayAvatarURL({ forceStatic: true }),
+    };
 
     const embed = new EmbedBuilder()
       .setTitle(t("play.title", { seconds: GUESS_TIME_SECONDS }))
       .setColor(Colors.White)
       .setDescription(game.censoredText)
-      .setFooter({
-        text: interaction.user.displayName,
-        iconURL: interaction.user.displayAvatarURL({ forceStatic: true }),
-      });
+      .setFooter(footer);
 
     const buttons = game.options.map((opt) =>
       new ButtonBuilder()
@@ -41,6 +42,30 @@ export const play: Command = {
         buttons
       );
 
-    await interaction.reply({ embeds: [embed], components: [row] });
+    const message = await interaction.reply({
+      embeds: [embed],
+      components: [row],
+    });
+
+    try {
+      await message.awaitMessageComponent({
+        filter: (i) => i.user.id === interaction.user.id,
+        time: GUESS_TIME_SECONDS * 1000,
+      });
+    } catch {
+      const timeoutEmbed = new EmbedBuilder()
+        .setTitle(t("timeout.title"))
+        .setFooter(footer)
+        .setColor(Colors.Red)
+        .setDescription(
+          t("timeout.description", {
+            answer: game.correctOption,
+            answerUrl: game.correctOptionUrl,
+            extract: game.originalText,
+          })
+        );
+
+      await message.edit({ embeds: [timeoutEmbed], components: [] });
+    }
   },
 };
